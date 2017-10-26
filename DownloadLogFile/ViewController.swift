82f413d345d5cff51c64dac5e1aee0434e3de252
Theint Kyizar <theintkyizar@Theints-Mac.local>
2017-10-27 00:46:22 +0800

    first

--- DownloadLogFile/ViewController.swift
+++ DownloadLogFile/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.isScrollEnabled = true
+        /*let label = UILabel(frame: CGRect(x:scrollView.contentOffset.x, y:scrollView.contentOffset.y, width:scrollView.frame.size.width, height:scrollView.frame.size.height))*/
+        let label = UILabel()
+        label.backgroundColor = UIColor(red: 0.988, green: 0.431, blue: 0.318, alpha: 1.0)
+        label.textColor = UIColor.white
         // Do any additional setup after loading the view, typically from a nib.
+        label.backgroundColor = UIColor(red: 0.988, green: 0.431, blue: 0.318, alpha: 1.0)
+        let fullscreen = UIScreen.main.bounds
+        self.scrollView.contentSize = CGSize(width:fullscreen.width, height:fullscreen.height*2.0)
+        self.scrollView.addSubview(label)
+        let maxSize = CGSize(width: 150, height: 1000)
+        let size = label.sizeThatFits(maxSize)
+        label.frame = CGRect(origin: CGPoint(x: 100, y: 100), size: size)
+        label.text = " Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game."
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-DownloadLogFileUITests/Pods-DownloadLogFileUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DownloadLogFileUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DownloadLogFileUITests
+@end
